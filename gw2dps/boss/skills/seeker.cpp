21b35d23181829c7e44a2bd10febaee47cd09d4a
Snake Layer <snakelayer@gmail.com>
2016-02-14 15:48:11 -0800

    update center seeker spawn location

--- gw2dps/boss/skills/seeker.cpp
+++ gw2dps/boss/skills/seeker.cpp
@@ -8,7 +8,7 @@ const std::list<Vector3> Seeker::spawnPositions = list_of
 	(Vector3(-5537, -20191, -2406)) // green
 	(Vector3(-3992, -20173, -2406)) // blue
 	(Vector3(-4770, -21516, -2407)) // red
-	(Vector3(-4772, -20634, -2401)); // center
+	(Vector3(-4749, -20607, -2401)); // center
 const float Seeker::SPAWN_LOCATION_TOLERANCE = 50.0f;
 
 bool Seeker::isSpawnPosition(Agent agent) {
