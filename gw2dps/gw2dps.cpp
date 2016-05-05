a7448ffa0b5097887c5aba17633386f1f96ae411
Snake Layer <snakelayer@gmail.com>
2016-05-05 01:21:18 -0700

    update character IsClone API

--- gw2dps/gw2dps.cpp
+++ gw2dps/gw2dps.cpp
@@ -1349,7 +1349,7 @@ void displayAgent(string prefix, Agent &agent, stringstream &ss) {
     ss << format(prefix + " alive: " + string(character.IsAlive() ? "yes" : "no") + "\n");
     ss << format(prefix + " downed: " + string(character.IsDowned() ? "yes" : "no") + "\n");
     ss << format(prefix + " monster: " + string(character.IsMonster() ? "yes" : "no") + "\n");
-    ss << format(prefix + " monster player clone: " + string(character.IsMonsterPlayerClone() ? "yes" : "no") + "\n");
+    ss << format(prefix + " monster player clone: " + string(character.IsClone() ? "yes" : "no") + "\n");
     ss << format(prefix + " valid: " + string(character.IsValid() ? "yes" : "no") + "\n");
     ss << format(prefix + " controlled: " + string(character.IsControlled() ? "yes" : "no") + "\n");
     ss << format(prefix + " downed: " + string(character.IsDowned() ? "yes" : "no") + "\n");
